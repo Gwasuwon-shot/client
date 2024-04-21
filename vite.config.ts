21c3864dbc2291be8e6b1c1faac1e6bca6bd661d
eunbenn <ds5bean23@gmail.com>
2024-04-21 22:55:18 +0900

    [ add ] 네이버 소셜 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { ProxyOptions } from "vite";
+
+// 프록시 설정
+const proxy: Record<string, string | ProxyOptions> = {
+  "/api": {
+    target: "https://openapi.naver.com",
+    changeOrigin: true,
+    rewrite: (path) => path.replace(/^\/api/, ""),
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +24,9 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    proxy: proxy,
+  },
   optimizeDeps: {
     exclude: ["js-big-decimal"],
   },
