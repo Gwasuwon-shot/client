9e1c1404d7bf71f6601eda27d4e973e3d3512d67
eunbeann <ds5bean23@gmail.com>
2024-04-23 17:51:34 +0900

    [ fix ] proxy 수정 테스트

--- src/api/naver/getNaverUserInfo.ts
+++ src/api/naver/getNaverUserInfo.ts
@@ -1,11 +1,29 @@
 import axios from "axios";
 
+const URL = `/proxy/v1/`;
+
+const api = axios.create({
+  baseURL: URL,
+  withCredentials: true,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+
 export async function getNaverUserInfo(accessToken: string) {
-  const response = await axios.get(`/api/v1/nid/me`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  console.log(response);
-  return response.data.response.id;
+  try {
+    const response = await api.get(`nid/me`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    console.log(response);
+    if (response.data && response.data.response) {
+      return response.data.response.id;
+    } else {
+      throw new Error('Invalid response structure');
+    }
+  } catch (error) {
+    console.error('Failed to fetch user info:', error);
+    return null;
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { ProxyOptions, defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // 프록시 설정
-// const proxy: Record<string, string | ProxyOptions> = {
-//   "/api": {
-//     target: "https://openapi.naver.com",
-//     changeOrigin: true,
-//     rewrite: (path) => path.replace(/^\/api/, ""),
-//   },
-// };
+const proxy: Record<string, string | ProxyOptions> = {
+  "/proxy": {
+    target: "https://openapi.naver.com",
+    changeOrigin: true,
+    rewrite: (path) => path.replace(/^\/proxy/, ""),
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
