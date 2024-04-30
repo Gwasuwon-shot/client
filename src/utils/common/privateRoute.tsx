86ff4f1c47c5b4aec45a29b91c3970f921e0eb82
eunbeann <ds5bean23@gamil.com>
2024-04-30 20:35:16 +0900

    [ fix ] 오타 수정

--- src/utils/common/privateRoute.tsx
+++ src/utils/common/privateRoute.tsx
@@ -1,10 +1,9 @@
 import { ReactElement } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { isCookieAuthenticated, isCookieNull, isLogin, isTempUser } from "./isLogined";
-import { getCookie } from "../../api/cookie";
+import { isCookieAuthenticated, isCookieNull, isLogin } from "./isLogined";
 
 interface PrivateRouteProps {
-  children?: ReactElement; // Router.tsx에서 PrivateRoute가 감싸고 있는 Componet Element
+  children?: ReactElement; // Router.tsx에서 PrivateRoute가 감싸고 있는 Component Element
   authentication: boolean; // true :인증을 반드시 해야하만 접속가능, false : 인증을 반드시 안해야만 접속 가능
 }
 
@@ -12,7 +11,7 @@ export default function PrivateRoute({ authentication }: PrivateRouteProps): any
   if (authentication) {
     // 인증이 반드시 필요한 페이지
     // 인증을 안했을 경우 로그인 페이지로, 했을 경우 해당 페이지로
-    !isLogin() || isCookieNull() || (isCookieAuthenticated() && alert("로그인 후 이용해주세요."));
+    // !isLogin() || isCookieNull() || (isCookieAuthenticated() && alert("로그인 후 이용해주세요."));
     return !isLogin() || isCookieNull() || isCookieAuthenticated() ? <Navigate to="/" /> : <Outlet />;
   }
 }
