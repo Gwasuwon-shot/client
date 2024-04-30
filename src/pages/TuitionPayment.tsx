0660a11030d851589e2536aee84c961fa66cd751
eunbeann <ds5bean23@gmail.com>
2024-04-30 17:25:13 +0900

    [ feat ] 은행 컴포넌트 추가

--- src/pages/TuitionPayment.tsx
+++ src/pages/TuitionPayment.tsx
@@ -1,18 +1,24 @@
 import { styled } from "styled-components";
+import BankListBottomSheet from "../components/tuitionPayment/BankListBottomSheet";
 import Footer from "../components/tuitionPayment/Footer";
 import Header from "../components/tuitionPayment/Header";
 import PaymentInput from "../components/tuitionPayment/PaymentInput";
+import useModal from "../hooks/useModal";
 
 export default function TuitionPayment() {
+  const { openModal } = useModal();
   return (
-    <PaymetWrapper>
-      <Header />
-      <PaymentInput />
-      <Footer />
-    </PaymetWrapper>
+    <>
+      {openModal && <BankListBottomSheet />}
+      <PaymentWrapper>
+        <Header />
+        <PaymentInput />
+        <Footer />
+      </PaymentWrapper>
+    </>
   );
 }
 
-const PaymetWrapper = styled.main`
+const PaymentWrapper = styled.main`
   overflow-y: scroll;
 `;
