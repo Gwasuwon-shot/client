e656184ccfcedbb91320a1f505adab52c48b1a65
eunbeann <ds5bean23@gamil.com>
2024-04-30 20:34:15 +0900

    [ remove ] 프록시 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import react from "@vitejs/plugin-react";
-import { ProxyOptions, defineConfig } from "vite";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-// 프록시 설정
-const proxy: Record<string, string | ProxyOptions> = {
-  "/proxy": {
-    target: "https://openapi.naver.com",
-    changeOrigin: true,
-    rewrite: (path) => path.replace(/^\/proxy/, ""),
-  },
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,9 +14,6 @@ export default defineConfig({
       },
     }),
   ],
-  server: {
-    proxy: proxy,
-  },
   optimizeDeps: {
     exclude: ["js-big-decimal"],
   },
