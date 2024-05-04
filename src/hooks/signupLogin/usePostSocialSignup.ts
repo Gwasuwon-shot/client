2095b567dc9eef19dbadc3e888282d5bf4ac210d
eunbeann <ds5bean23@gamil.com>
2024-05-04 20:02:10 +0900

    [ fix ] 로그인 버그 수정

--- src/hooks/signupLogin/usePostSocialSignup.ts
+++ src/hooks/signupLogin/usePostSocialSignup.ts
@@ -1,29 +1,45 @@
 import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
-import { setCookie } from "../../api/cookie";
+import { removeCookie, setCookie } from "../../api/cookie";
 import { postSocialSignUp } from "../../api/signUp/postSocialSignUp";
 import { NewSocialUserTypes } from "../../type/SignUp/newUserDataType";
-import { isCookieAuthenticated, isCookieNull, isLogin } from "../../utils/common/isLogined";
+import { isLogin } from "../../utils/common/isLogined";
+
+interface tempSignResType {
+  accessToken: string;
+  refreshToken: string;
+  user: {
+    name: string;
+    role: "부모님" | "선생님";
+  };
+}
 
 export default function usePostSocialSignup(newUser: NewSocialUserTypes) {
   const navigate = useNavigate();
+
+  const setToken = (data: tempSignResType) => {
+    removeCookie("tempToken");
+
+    setCookie("accessToken", data.accessToken, {
+      secure: true,
+      path: "/",
+    });
+
+    setCookie("refreshToken", data.refreshToken, {
+      secure: true,
+      path: "/",
+    });
+  };
 
   const mutation = useMutation({
     mutationFn: async () => {
       return await postSocialSignUp(newUser);
     },
     onSuccess: (data) => {
-      setCookie("accessToken", data.data.accessToken, {
-        secure: true,
-      });
-      setCookie("refreshToken", data.data.refreshToken, {
-        secure: true,
-      });
+      setToken(data.data.data);
 
-      if (isLogin() && !isCookieNull() && !isCookieAuthenticated()) {
-        setTimeout(() => {
-          navigate("/home");
-        }, 3000);
+      if (isLogin()) {
+        navigate("/home");
       }
     },
     onError: (error) => {
