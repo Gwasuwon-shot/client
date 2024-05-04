576fbd6d37e93921bcb41a6279c0d264f18cb0f9
eunbeann <ds5bean23@gamil.com>
2024-05-04 20:09:48 +0900

    [ fix ] 번호 인증 살리기

--- src/components/signup/UserPhone.tsx
+++ src/components/signup/UserPhone.tsx
@@ -3,7 +3,6 @@ import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { newSocialUser } from "../../atom/signup/signup";
 import { AUTH_CODE_PATTERN } from "../../core/signup/regex";
-import usePostSocialSignup from "../../hooks/signupLogin/usePostSocialSignup";
 import useReverseRole from "../../hooks/signupLogin/useReverseRole";
 import useSendValidNumber from "../../hooks/signupLogin/useSendValidNumber";
 import useValidatePhone from "../../hooks/signupLogin/useValidatePhone";
@@ -55,14 +54,14 @@ export default function UserPhone() {
   });
 
   const handleClickSend = () => {
-    // sendValidNumber.mutate(digitNumber);
-    successToSendCode();
+    sendValidNumber.mutate(digitNumber);
+    // successToSendCode();
   };
 
-  const postSocialSignUp = usePostSocialSignup(newUser);
+  // const postSocialSignUp = usePostSocialSignup(newUser);
   const handleClickConfirm = () => {
-    // validatePhone.mutate({ number: digitNumber, validCode: validCode });
-    postSocialSignUp.mutate();
+    validatePhone.mutate({ number: digitNumber, validCode: validCode });
+    // postSocialSignUp.mutate();
   };
 
   return (
