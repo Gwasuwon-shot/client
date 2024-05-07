46317a2c816df1ef429c7736ae60c7e41b9a5d90
eunbeann <ds5bean23@gmail.com>
2024-05-07 10:21:27 +0900

    [ typo ] 오타 수정

--- src/components/common/NoCheckPageAttendanceButton.tsx
+++ src/components/common/NoCheckPageAttendanceButton.tsx
@@ -8,13 +8,13 @@ export default function NoCheckPageAttendanceButton(props: NoCheckPageAttendance
   const { onClick } = props;
 
   return (
-    <AttendaceCheckButtonBox type="button" onClick={onClick}>
+    <AttendanceCheckButtonBox type="button" onClick={onClick}>
       <h1>출결 체크</h1>
-    </AttendaceCheckButtonBox>
+    </AttendanceCheckButtonBox>
   );
 }
 
-const AttendaceCheckButtonBox = styled.button`
+const AttendanceCheckButtonBox = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
