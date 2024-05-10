663e3e48b2a99b3f1cd735253fe63fe2973a2100
HAE2NI <hae0_02ni@ewhain.net>
2024-05-10 22:39:28 +0900

    [ feat ] App.jsx에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import Router from "./Router";
 import "./core/notification/settingFCM";
 import { GlobalStyle } from "./style/globalStyle";
 import { theme } from "./style/theme";
+import REACTGA from "react-ga4";
 
 export default function App() {
+  if (import.meta.env.REACT_APP_GOOGLE_ANALYTICS) {
+    REACTGA.initialize(import.meta.env.REACT_APP_GOOGLE_ANALYTICS);
+  }
+
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
