2a5febde4466f2917d1b811ad53a88577bb1d04c
eunbeann <ds5bean23@gmail.com>
2024-05-11 21:15:23 +0900

    [ feat ] Sentry 세팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Suspense } from "react";
@@ -41,49 +42,51 @@ import PrivateRoute from "./utils/common/privateRoute";
 export default function Router() {
   return (
     <BrowserRouter>
-      <ErrorBoundary FallbackComponent={fallbackRender}>
-        <Suspense fallback={<Loading />}>
-          <Routes>
-            <Route path="/" element={<OnBoarding />} />
-            <Route path="/landing" element={<Landing />} />
-            <Route path="/on-boarding" element={<OnBoarding />} />
-            <Route path="/welcome" element={<WelcomeSignup />} />
-            <Route path="/:lessonId" element={<ConnectParentsAndTeacher />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/lesson-share" element={<LessonShare />} />
-            <Route path="/kakao-redirect" element={<KakaoRedirect />} />
-            <Route path="/naver-redirect" element={<NaverRedirect />} />
+      <Sentry.ErrorBoundary fallback={fallbackRender}>
+        <ErrorBoundary FallbackComponent={fallbackRender}>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="/" element={<OnBoarding />} />
+              <Route path="/landing" element={<Landing />} />
+              <Route path="/on-boarding" element={<OnBoarding />} />
+              <Route path="/welcome" element={<WelcomeSignup />} />
+              <Route path="/:lessonId" element={<ConnectParentsAndTeacher />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/lesson-share" element={<LessonShare />} />
+              <Route path="/kakao-redirect" element={<KakaoRedirect />} />
+              <Route path="/naver-redirect" element={<NaverRedirect />} />
 
-            <Route element={<PrivateRoute authentication={true} />}>
-              <Route path="/home" element={<Home />} />
-              <Route path="/complete-check-attendance" element={<CompleteCheckAttendance />} />
-              <Route path="/manage-lesson" element={<ManageLessonMain />} />
-              <Route path="/manage-lesson/:manageLessonId" element={<ManageLessonDetail />} />
-              <Route path="/register-payment/:manageLessonId" element={<RegisterPayment />} />
-              <Route path="/parent-calendar" element={<ParentCalendar />} />
-              <Route path="/register-lesson" element={<RegisterLesson />} />
-              <Route path="/regular-lesson-cycle" element={<RegularLessonCycle />} />
-              <Route path="/regular-lesson-date" element={<RegularLessonDate />} />
-              <Route path="/regular-lesson" element={<RegularLesson />} />
-              <Route path="/register-calendar" element={<RegisterCalendar />} />
-              <Route path="/lesson-detail/:lessonId" element={<LessonDetail />} />
-              <Route path="/time-picker" element={<TimePickerPage />} />
-              <Route path="/schedule" element={<ChangeSchedule />} />
-              <Route path="/tuition-payment" element={<TuitionPayment />} />
-              <Route path="/register-complete" element={<LessonRegisterComplete />} />
-              <Route path="/lesson-connect" element={<LessonConnect />} />
+              <Route element={<PrivateRoute authentication={true} />}>
+                <Route path="/home" element={<Home />} />
+                <Route path="/complete-check-attendance" element={<CompleteCheckAttendance />} />
+                <Route path="/manage-lesson" element={<ManageLessonMain />} />
+                <Route path="/manage-lesson/:manageLessonId" element={<ManageLessonDetail />} />
+                <Route path="/register-payment/:manageLessonId" element={<RegisterPayment />} />
+                <Route path="/parent-calendar" element={<ParentCalendar />} />
+                <Route path="/register-lesson" element={<RegisterLesson />} />
+                <Route path="/regular-lesson-cycle" element={<RegularLessonCycle />} />
+                <Route path="/regular-lesson-date" element={<RegularLessonDate />} />
+                <Route path="/regular-lesson" element={<RegularLesson />} />
+                <Route path="/register-calendar" element={<RegisterCalendar />} />
+                <Route path="/lesson-detail/:lessonId" element={<LessonDetail />} />
+                <Route path="/time-picker" element={<TimePickerPage />} />
+                <Route path="/schedule" element={<ChangeSchedule />} />
+                <Route path="/tuition-payment" element={<TuitionPayment />} />
+                <Route path="/register-complete" element={<LessonRegisterComplete />} />
+                <Route path="/lesson-connect" element={<LessonConnect />} />
 
-              <Route path="/myPage" element={<MyPage />} />
-              <Route path="/no-attendance-check" element={<NoAttendanceCheck />} />
-              <Route path="/lesson-info/:lessonId" element={<LessonInfo />} />
-              <Route path="/edit-lessonschedule" element={<EditSchedule />} />
-              <Route path="/alert" element={<AllowAlert />} />
-              <Route path="/tree" element={<AfterSignup />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </ErrorBoundary>
+                <Route path="/myPage" element={<MyPage />} />
+                <Route path="/no-attendance-check" element={<NoAttendanceCheck />} />
+                <Route path="/lesson-info/:lessonId" element={<LessonInfo />} />
+                <Route path="/edit-lessonschedule" element={<EditSchedule />} />
+                <Route path="/alert" element={<AllowAlert />} />
+                <Route path="/tree" element={<AfterSignup />} />
+              </Route>
+            </Routes>
+          </Suspense>
+        </ErrorBoundary>
+      </Sentry.ErrorBoundary>
     </BrowserRouter>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import { CookiesProvider } from "react-cookie";
@@ -12,6 +13,18 @@ const queryClient = new QueryClient({
       retry: 0,
     },
   },
+});
+
+Sentry.init({
+  dsn: process.env.NODE_ENV === "production" ? import.meta.env.VITE_APP_SENTRY_DSN : false,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/tutice\.o-r\.kr/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want t change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 declare global {
